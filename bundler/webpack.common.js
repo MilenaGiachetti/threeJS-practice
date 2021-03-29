be3b874694051ced8c07a027944af80b2abac786
Milena Giachetti <giachettimilena@gmail.com>
2021-03-29 20:03:13 -0300

    Add 3d text example

--- bundler/webpack.common.js
+++ bundler/webpack.common.js
@@ -23,12 +23,13 @@ const populateHtmlPlugins = (pagesArray) => {
     return res;
 }
 
-const pages = populateHtmlPlugins(["cube_responsive", "add_model", "donut", "hello_cube", "hello_cubes", "primitives"]);
+const pages = populateHtmlPlugins(["cube_responsive", "add_model", "donut", "hello_cube", "hello_cubes", "primitives", "text"]);
 
 module.exports = {
     entry: {
         index: path.resolve(__dirname, '../src/script.js'),
-        add_model: path.resolve(__dirname, '../src/pages/add_model/script.js')
+        add_model: path.resolve(__dirname, '../src/pages/add_model/script.js'),
+        text: path.resolve(__dirname, '../src/pages/text/script.js')
     },
     output: {
         publicPath: isProduction ? '/threeJS-practice/' : '/',
