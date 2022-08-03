741f6fb488bde2312bb3d4c75756605fbf3884c9
MilenaGiachetti <giachettimilena@gmail.com>
2022-08-03 00:45:41 -0300

    Move planet according to keyboard input & refactor world textures settings

--- src/Components/World.tsx
+++ src/Components/World.tsx
@@ -1,17 +1,17 @@
-import { useLoader, useFrame } from '@react-three/fiber';
-import { TextureLoader } from 'three/src/loaders/TextureLoader'
-import color from '@assets/images/textures/grass/base_color.jpg';
-import ambient_occlusion from '@assets/images/textures/grass/ambient_occlusion.jpg';
-import height from '@assets/images/textures/grass/height.png';
-import normal from '@assets/images/textures/grass/normal.jpg';
-import roughness from '@assets/images/textures/grass/roughness.jpg';
-
 import * as THREE from 'three';
 import { useRef } from 'react';
-
+import { useLoader, useFrame } from '@react-three/fiber';
+import { TextureLoader } from 'three/src/loaders/TextureLoader'
+import color from '@assets/images/textures/watermelon/base_color.jpg';
+import ambient_occlusion from '@assets/images/textures/watermelon/ambient_occlusion.jpg';
+import normal from '@assets/images/textures/watermelon/normal.jpg';
+import roughness from '@assets/images/textures/watermelon/roughness.jpg';
+import { useStore } from '@zustand/store';
 
 function World() {
-  const myMesh = useRef<any>();
+  const worldMesh = useRef<any>();
+
+  const { forward, backward, left, right } = useStore((state) => state)
 
   const textures = useLoader(TextureLoader, [
     color,
@@ -21,31 +21,34 @@ function World() {
   ])
 
   const [colorMap, normalMap, roughnessMap, aoMap] = textures;
-  colorMap.repeat.set(5, 5);
-  colorMap.wrapS = THREE.RepeatWrapping;
-  colorMap.wrapT = THREE.RepeatWrapping;
 
-  normalMap.repeat.set(5, 5);
-  normalMap.wrapS = THREE.RepeatWrapping;
-  normalMap.wrapT = THREE.RepeatWrapping;
+  textures.forEach((texture) => {
+    texture.repeat.set(2, 2);
+    texture.wrapS = THREE.RepeatWrapping;
+    texture.wrapT = THREE.RepeatWrapping;
+  })
 
-  roughnessMap.repeat.set(5, 5);
-  roughnessMap.wrapS = THREE.RepeatWrapping;
-  roughnessMap.wrapT = THREE.RepeatWrapping;
-
-  aoMap.repeat.set(5, 5);
-  aoMap.wrapS = THREE.RepeatWrapping;
-  aoMap.wrapT = THREE.RepeatWrapping;
-
-
-  // useFrame(({ clock }) => {
-  //   myMesh.current.rotation.x = clock.getElapsedTime() / 20;
-  //   myMesh.current.rotation.y = clock.getElapsedTime() / 20
-  // })
+  useFrame(() => {
+    if (forward) {
+      worldMesh.current.rotation.x += 0.01
+    }
+    if (backward) {
+      worldMesh.current.rotation.x -= 0.01
+    }
+    if (left) {
+      worldMesh.current.rotation.z += 0.01
+    }
+    if (right) {
+      worldMesh.current.rotation.z -= 0.01
+    }
+  })
 
   return (
-    <mesh ref={myMesh} position={[0, -12, 0]}
-      rotation={new THREE.Euler(-Math.PI / 4, 0, -Math.PI / 2)}>
+    <mesh
+      ref={worldMesh}
+      position={[0, -12, 0]}
+    // rotation={new THREE.Euler(0, 0, 0)}
+    >
       <sphereGeometry args={[10, 64, 64]} />
       <meshStandardMaterial
         map={colorMap}
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,27 +1,26 @@
 import { Canvas } from '@react-three/fiber';
 import SceneEnvironment from '@Components/Environment';
-import Floor from '@Components/Floor';
 import World from '@Components/World';
 import Duck from '@Components/Duck';
 import Camera from '@Components/Camera';
 
 import { OrbitControls } from '@react-three/drei';
 import * as THREE from 'three';
+import { useKeyboardMovement } from '@hooks/useKeyboardMovement';
 
 export default function Home() {
+	useKeyboardMovement();
 
 	return (
 		<Canvas>
 			<OrbitControls />
+			<Camera fov={75} position={new THREE.Vector3(0, 2, 5)} />
+			<pointLight position={[10, 10, 10]} intensity={0.5} />
 			<ambientLight intensity={0.2} />
 			<ambientLight color='#FD5E53' intensity={0.2} />
 			<SceneEnvironment />
 			<Duck position={[0, -2, 0,]} />
 			<World />
-			<Camera fov={75} position={new THREE.Vector3(0, 2, 5)} />
-			{/* <Floor position={[0, -1, 0]} rotation={new THREE.Euler(-Math.PI / 2, 0, 0)} /> */}
-			<pointLight position={[10, 10, 10]} />
-			{/* <Box position={[0, 0, 0]} /> */}
 		</Canvas>
 	)
 }
